252693eeb27e5f3eea50b21362d063ec25f2a463
Long Zheng <long.zheng@gmail.com>
2018-08-05 11:37:01 +1000

    Add d.ts bundling to webpack plugins
    Append "export as namespace" to d.ts

--- generators/app/templates/webpack.config.js
+++ generators/app/templates/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const fs = require('fs')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
 
@@ -28,6 +29,30 @@ module.exports = {
     new CleanWebpackPlugin(['dist']),
     new UglifyJSPlugin({
       sourceMap: true
-    })
+    }),
+    new DtsBundlePlugin()
   ]
 }
+
+function DtsBundlePlugin() {}
+DtsBundlePlugin.prototype.apply = function(compiler) {
+    compiler.plugin('done', function() {
+        var dts = require('dts-bundle');
+
+        dts.bundle({
+            name: '<%= props.libraryName.umd %>',
+            main: 'dist/index.d.ts',
+            out: 'index.d.ts',
+            removeSource: true,
+            outputAsModuleFolder: true,
+            verbose: true
+        });
+
+        // Add 'export as namespace' to declaration file for the UMD module
+        fs.appendFile('dist/index.d.ts', 'export as namespace <%= props.libraryName.umd %>', (err) => {
+          if (err) {
+              throw err
+          }
+      })
+    });
+};
